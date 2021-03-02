319a36ca0fc31be9da4a51a3370685fb3251c391
Alexander Ippatiev <33111889+alexanderippatiev@users.noreply.github.com>
2021-03-03 01:35:46 +0900

    btn & spacing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <base-button color="black">Primary Button</base-button>
+  <base-button @click="clickHandler">First Button</base-button>
+  <base-button class="mt-8" @click="clickHandler">Second Button</base-button>
 </template>
 
 <script>
@@ -9,11 +10,14 @@ export default {
   components: {
     BaseButton,
   },
+  methods: {
+    clickHandler(t) {
+      alert(`${t.target.textContent} clicked!`);
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-body {
-  font-size: 16px;
-}
+@import '@/assets/style/style.scss';
 </style>
--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,36 +1,14 @@
 <script>
 import { h } from 'vue';
 export default {
-  props: {
-    color: {
-      type: String,
-      default: 'black',
-    },
-  },
   render() {
     return h(
       'button',
       {
-        style: { backgroundColor: this.color },
         class: ['bbb-btn'],
-        onClick: this.$emit('click'),
       },
       this.$slots.default()
     );
   },
 };
 </script>
-<style lang="scss">
-.bbb-btn {
-  display: block;
-  border-radius: 8px;
-  padding: 0 24px;
-  align-items: center;
-  border: none;
-  width: 200px;
-  line-height: 52px;
-  min-width: 200px;
-  color: white;
-  font-size: inherit;
-}
-</style>
