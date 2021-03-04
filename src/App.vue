79d36230100105cb1ff45f36fc62c41f4bab1314
Alexander Ippatiev <33111889+alexanderippatiev@users.noreply.github.com>
2021-03-05 03:23:32 +0900

    update for select elements

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <base-button @click="clickHandler">First Button</base-button>
-  <base-button class="mt-8" @click="clickHandler">Second Button</base-button>
+  <div class="container">
+    <base-button @click="clickHandler">First Button</base-button>
+    <base-select @input="selectHandler" :options="selects" class="ml-16" />
+  </div>
 </template>
 
 <script>
 import BaseButton from './components/BaseButton.vue';
+import BaseSelect from './components/BaseSelect.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      selects: [
+        { id: 0, value: 'Dropdown Item' },
+        { id: 1, value: 'Dropdown Item' },
+      ],
+    };
+  },
   components: {
     BaseButton,
+    BaseSelect,
   },
   methods: {
     clickHandler(t) {
       alert(`${t.target.textContent} clicked!`);
+    },
+    selectHandler(t) {
+      console.log(`${t.target.value} selected!`);
     },
   },
 };
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,14 @@
+<template>
+  <select class="bbb-select">
+    <option v-for="op in options" :key="op.id" :value="op.id"
+      >{{ op.value }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  props: {
+    options: Array,
+  },
+};
+</script>
